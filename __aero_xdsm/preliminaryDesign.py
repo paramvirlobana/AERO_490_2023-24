48257b55f72a3e8767b101dbe100024ba61e2798
Paramvir Lobana <85321607+paramvirlobana@users.noreply.github.com>
2023-10-19 11:50:16 -0400

    pyxdsm_prelim

--- __aero_xdsm/preliminaryDesign.py
+++ __aero_xdsm/preliminaryDesign.py
@@ -6,7 +6,7 @@ x = XDSM(use_sfmath=True)
 
 
 ## ADD ALL SYSTEMS VARIABLE FUNCTIONS
-cd = "constraint_diagram"
+cd = "conceptial_design"
 perf_para = "perf_para"
 ref_wing_des = "ref_wing_des"
 tail_size = "tail_size"
@@ -14,31 +14,48 @@ fuse_size = "fuse_size"
 UAV_geom = "UAV_geom"
 
 ## ADD ALL THE SYSTEMS
-x.add_system(cd, FUNC, "Constraint \; Diagram")
-x.add_system(perf_para, FUNC, "Performance \; Parameters")
-x.add_system(ref_wing_des, FUNC, "Refined \; Wing \; Design")
+x.add_system(cd, FUNC, "Conceptual \; Design")
+x.add_system(perf_para, FUNC, "Performance \; Analysis")
+x.add_system(ref_wing_des, FUNC, "Wing \ Sizing")
 x.add_system(tail_size, FUNC, "Tail \; Sizing")
 x.add_system(fuse_size, FUNC, "Fuselage \; Sizing")
-x.add_system(UAV_geom, FUNC, "UAV \; Configuration")
+x.add_system(UAV_geom, FUNC, "Preliminary \ Design")
 
 
 ## ADD ALL THE INPUTS AND OUTPUTS
 x.add_input(tail_size, (r"\text{Tail Config from Baseline}",
                          r"\bar{V}_H, \bar{V}_V, S_{h}, S_{v}, l_{opt},\text{etc}"),stack=True)
 
-x.add_input(fuse_size, (r"\text{Battery Volume}",
-                         r"\text{Fuse Config from Baseline}",
-                         r"\text{Mission Specific Requirments}"),
+x.add_input(fuse_size, (r"\text{Payload Dimensions}",
+                         r"\text{Design Requirements/Constraints}",
+                         r"\text{Mission Specific Requirements}"),
                          stack=True)
 
-
+x.add_input(perf_para, r"\text{Initial Guesses}")
+#x.add_input(ref_wing_des, r"\text{Airfoil}")
 
 
 # ADD ALL THE CONNECTIONS
 x.connect(cd, perf_para, r"W_{TO}")
+x.connect(perf_para, tail_size, r"\text{Aircraft CG}")
 x.connect(cd, ref_wing_des, r"S_{ref}, T, P, C_{L_{max}}")
-x.connect(ref_wing_des, tail_size, r"C_{L_{C}}")
+x.connect(ref_wing_des, tail_size, r"C_{L_{C}}, C_{m}, MAC, i_w")
+x.connect(ref_wing_des, fuse_size, r"\text{Wing Placement}")
+x.connect(tail_size, fuse_size, "Tail \ Placement, L_f")
+x.connect(ref_wing_des, perf_para, r"\text{Stability Analysis}")
+x.connect(tail_size, perf_para, r"\text{Stability Analysis}")
+x.connect(UAV_geom, perf_para, r"\text{Revised Requirements}")
+x.connect(fuse_size, UAV_geom, r"Fuselage\ Geometry")
+x.connect(fuse_size, perf_para, r"C_{L_{UAV}}, C_{D_{UAV}}")
+x.connect(perf_para, ref_wing_des, r"Design \ Requirements")
+x.connect(UAV_geom, ref_wing_des, r"UAV \ Lift ")
 
+# Results
+x.connect(ref_wing_des, UAV_geom, r"Wing \ Geometry")
+x.connect(tail_size, UAV_geom, r"Tail\ Geometry")
+x.connect(fuse_size, UAV_geom, r"Fuselage\ Geometry")
+
+x.add_output(UAV_geom, "Outer \ Mold \ Line ")
 ## Write PDF
 x.write("pdXDSM")
 
